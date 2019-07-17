813db1575c0acf576293a6ed544a14cb298c2b6e
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-07-17 11:47:49 +0200

    new module: RGB Colors

---
+++ Tegami/Color.hs
@@ -0,0 +1,32 @@
+module Tegami.Color where
+
+import Tegami.Core
+
+
+black :: RGB
+black = (0, 0, 0)
+
+white :: RGB
+white = (1, 1, 1)
+
+gray :: RGB
+gray = (0.5, 0.5, 0.5)
+
+red :: RGB
+red = (1, 0, 0)
+
+green :: RGB
+green = (0, 1, 0)
+
+blue :: RGB
+blue = (0, 0, 1)
+
+yellow :: RGB
+yellow = (1, 1, 0)
+
+magenta :: RGB
+magenta = (1, 0, 1)
+
+cyan :: RGB
+cyan = (0, 1, 1)
+
