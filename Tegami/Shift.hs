09f1869ad4d81098aec45a0967369fa7922a2ebf
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-07-23 11:54:24 +0200

    new shift: shiftConc - concentric shift

--- Tegami/Shift.hs
+++ Tegami/Shift.hs
@@ -1,12 +1,13 @@
 module Tegami.Shift where
 
-import Tegami.Transform (trans, rot)
+import Tegami.Transform (trans, rot, zoom)
 import Tegami.Core (twopi)
 
 shift op t img = op img $ img . t
 shiftH op x = shift op $ trans (x, 0)
 shiftV op y = shift op $ trans (y, 0)
 shiftRot op theta = shift op $ rot theta
+shiftConc op ratio = shift op $ zoom ratio
 
 around op radius n p = foldr1 op ps
   where ps = [p . trans (x i, y i) | i <- [0..n-1]]
