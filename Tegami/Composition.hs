17c0441902eba65f2de50199fbffc55380c8c588
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-10-06 10:29:02 +0200

    new composition: concentric_tile

--- Tegami/Composition.hs
+++ Tegami/Composition.hs
@@ -7,3 +7,7 @@ tile f (x, y) = patch (patch_x, patch_y)
         col = round x
         patch_x = x - fromIntegral col
         patch_y = y - fromIntegral row
+
+concentric_tile f (x,y) = patch (x,y)
+    where patch = f ring
+          ring = max (round $ abs x) (round $ abs y)
