884f2ba0d8659c6def7fe43c472ccfeb80fe77fb
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-03-08 07:52:34 +0100

    zoom in 032 so the hyperbolic disk boundary is outside the frame

--- src/032_hyperbolic_stars.hs
+++ src/032_hyperbolic_stars.hs
@@ -5,6 +5,6 @@ import Tegami.Composition (tile)
 
 tiledStars = tile (\_ _ -> star 6 0.9 0.4 . zoom 0.45)
 
-image_032 = circleLimit (tiledStars . zoom 0.3) (const False)
+image_032 = circleLimit (tiledStars . zoom 0.3) (const False) . zoom 2.5
 
 main = autoImage image_032
