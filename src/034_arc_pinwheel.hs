4974b250b4a2940f8d4c94b5079d7a8d068b499c
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-03-08 07:48:39 +0100

    shorten arc span in 034 for more separation between spokes
    Reduce half-angle from pi/10 to pi/16, increasing the gap between
    adjacent arcs from ~9° to ~22°.

--- src/034_arc_pinwheel.hs
+++ src/034_arc_pinwheel.hs
@@ -3,7 +3,7 @@ import Tegami.Shape (arc)
 import Tegami.Transform (zoom, rot)
 import Control.Applicative (liftA2)
 
-spoke a = arc 0.75 0.08 (pi/10) . rot a . zoom 0.9
+spoke a = arc 0.75 0.08 (pi/16) . rot a . zoom 0.9
 
 image_034 = foldr1 (liftA2 (||)) [spoke (fromIntegral i * pi/4) | i <- [0..7]]
 
